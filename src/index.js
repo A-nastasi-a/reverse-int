4f2fadafad8430c562dc691f226011eccf865efb
A-nastasi-a <evsihneyeva@gmail.com>
2022-06-24 19:33:36 +0400

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n >= 0) {
+        return Number(String(n).split('').reverse().join(''));
+    } else {
+        return Number(String(n).slice(1).split('').reverse().join(''));
+    }
+
 }
